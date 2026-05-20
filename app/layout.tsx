f27d640dbc5637c99217437b1f50c1bd5d0d9b12
Gautam Kumar B <gautamkumarbudida18@gmail.com>
2026-05-20 12:59:25 +0530

    Added Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { ClerkProvider } from "@clerk/nextjs";
 import { ConvexClientProvider } from "./ConvexClientProvider";
 
@@ -30,6 +31,7 @@ export default function RootLayout({
           <ConvexClientProvider>
             {children}
             <Analytics />
+            <SpeedInsights />
           </ConvexClientProvider>
         </body>
       </html>
